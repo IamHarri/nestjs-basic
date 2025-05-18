a869dde728ef4221fb538b263575505da482dd4c
Long Ly <long18161245@gmail.com>
2025-05-18 16:36:24 +0700

    feat: add validation for dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,11 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @IsEmail({},{message: 'Email is not valid' })
+  @IsNotEmpty({message: 'Email is required' })
   email: string;
+
+  @IsNotEmpty({message: 'Password is required' })
   password: string;
   name: string;
   address: string;
