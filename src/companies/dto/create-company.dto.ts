3ec1b2d23581263dcb76b4978da66251de840071
Long Ly <long18161245@gmail.com>
2025-05-25 16:55:27 +0700

    feat: add company module

---
+++ src/companies/dto/create-company.dto.ts
@@ -0,0 +1,13 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateCompanyDto {
+
+  @IsNotEmpty({message: 'Name is required' })
+  name: string;
+
+  @IsNotEmpty({message: 'Address is required' })
+  address: string;
+  
+  @IsNotEmpty({message: 'Description is required' })
+  description: string;
+}
