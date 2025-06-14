4650d0b52ab7df2f1a14d42c4bce806b47eb08e1
Long Ly <long18161245@gmail.com>
2025-06-14 17:37:18 +0700

    feat: add versioning for api

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { TransformInterceptor } from './core/transform.interceptor';
 
@@ -14,10 +14,19 @@ async function bootstrap() {
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  // Set the view engine to EJS
   app.setViewEngine('ejs');
+
+  // Use global pipes to validate incoming requests
   app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalInterceptors(new TransformInterceptor());
+  
+  // Use global guards to protect routes that require authentication
   app.useGlobalGuards( new JwtAuthGuard( reflector ) );
+
+  // Config interceptor to transform response data, so that it can be consistent across the application
+  app.useGlobalInterceptors(new TransformInterceptor(reflector));
+
+  // config cors
   app.enableCors(
     {
       "origin": "*",
@@ -26,6 +35,14 @@ async function bootstrap() {
       "optionsSuccessStatus": 204
     }
   );
+
+  // config versioning
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    defaultVersion: ['1', '2'],
+    type: VersioningType.URI,
+  });
+
   await app.listen(configService.get<number>('PORT'));
 }
 bootstrap();
